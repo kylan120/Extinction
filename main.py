9719117db5201bf550a82ed9f3b18501819c3bc6
kylan120 <larsonkylan@gmail.com>
2023-10-25 10:45:36 -0500

    Made some small changes

--- main.py
+++ main.py
@@ -8,16 +8,13 @@ from player import *
 # This is the main class for the game
 class Game:
     def __init__(self):
+        self.map = Map(self)
+        self.player = Player(self)
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         pygame.display.set_caption("Extinction")
         self.clock = pygame.time.Clock()
         self.delta_time = 1
-        self.new_game()
-
-    def new_game(self):
-        self.map = Map(self)
-        self.player = Player(self)
 
     def update(self):
         self.player.update()
@@ -36,13 +33,11 @@ class Game:
                 pygame.quit()
                 sys.exit()
 
-
     def main(self):
         while True:
             self.check_events()
             self.player.update()  # Call the update method here
             self.draw()  # Call the draw method here
-
 
 
 if __name__ == '__main__':
