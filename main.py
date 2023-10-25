89114bdde0dcb0bf88d7f72d5eb5825cf3521697
kylan120 <larsonkylan@gmail.com>
2023-10-25 10:48:38 -0500

    Added some comments

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Name: Kylan, Harry, and Nick
+#Descripiton: This is the main class, that implements all the classes to the game
+#Date: 10/25/2023
+
 import pygame
 import sys
 from Settings import *
@@ -16,30 +20,34 @@ class Game:
         self.clock = pygame.time.Clock()
         self.delta_time = 1
 
+#This updates the player
     def update(self):
         self.player.update()
         self.draw()
         self.delta_time = self.clock.tick(FPS)
 
+#This will draw out the player
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.map.draw()
         self.player.draw()
         pygame.display.flip()
 
+#This check if the user hits any escape button to close the program
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
 
+#This happens while everything is running it'll run the program
     def main(self):
         while True:
             self.check_events()
-            self.player.update()  # Call the update method here
-            self.draw()  # Call the draw method here
+            self.player.update()
+            self.draw()
 
-
+#This sets the main to the game
 if __name__ == '__main__':
     game = Game()
     game.main()
