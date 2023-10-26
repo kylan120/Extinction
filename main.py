6bbf29f3ab1bd354ae267f5bdc7f6c8057acff48
kylan120 <larsonkylan@gmail.com>
2023-10-25 20:30:25 -0500

    Start over and added what the video says

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
-import pygame as pg
+import pygame
 import sys
-from map import *
 from Settings import *
-
+from map import *
+from player import *
 
 
 class Game:
     def __init__(self):
-        pg.init()
-        pg.mouse.set_visible(False)
-        self.screen = pg.display.set_mode(RES)
-        pg.event.set_grab(True)
-        self.clock = pg.time.Clock()
+        pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.clock = pygame.time.Clock()
         self.delta_time = 1
-        self.global_trigger = False
-        self.global_event = pg.USEREVENT + 0
-        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
-
+        self.player = Player(self)
 
     def update(self):
         self.player.update()
-        self.raycasting.update()
-        self.object_handler.update()
-        self.weapon.update()
-        pg.display.flip()
+        pygame.display.set_caption("Extinction")
+        pygame.display.flip()
         self.delta_time = self.clock.tick(FPS)
-        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        # self.screen.fill('black')
-        self.object_renderer.draw()
-        self.weapon.draw()
-        # self.map.draw()
-        # self.player.draw()
+        self.screen.fill((0, 0, 0))
+        self.map.draw()
+        self.player.draw()
 
     def check_events(self):
-        self.global_trigger = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                pg.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
                 sys.exit()
-            elif event.type == self.global_event:
-                self.global_trigger = True
-            self.player.single_fire_event(event)
 
     def run(self):
         while True:
             self.check_events()
-            self.update()
             self.draw()
+            self.update()
 
 
 if __name__ == '__main__':
