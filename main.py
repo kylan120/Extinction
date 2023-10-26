59c2ae80fa1074f82766bb7baec2d22b2d6f201b
kylan120 <larsonkylan@gmail.com>
2023-10-25 19:27:17 -0500

    Changed stuff

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
-import pygame
+import pygame as pg
 import sys
+from map import *
 from Settings import *
-from map import Map
-from player import Player
-from raycasting import *
 
-# This is the main class for the game
+
+
 class Game:
     def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(RES)
-        self.clock = pygame.time.Clock()
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.screen = pg.display.set_mode(RES)
+        pg.event.set_grab(True)
+        self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
-        pygame.display.set_caption("Extinction")  # Set the window title
-
-    # This updates the game state
-    def update(self):
-        self.player.update()
-        self.raycasting.update()
-        self.delta_time = self.clock.tick(FPS)
 
     def new_game(self):
         self.map = Map(self)
-        self.player = Player(self)
-        self.raycasting = RayCasting(self)
 
-    # This will draw the game elements
+
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
     def draw(self):
-        self.screen.fill((0, 0, 0))
-        self.map.draw()
-        self.player.draw()
-        pygame.display.flip()
+        # self.screen.fill('black')
+        self.object_renderer.draw()
+        self.weapon.draw()
+        # self.map.draw()
+        # self.player.draw()
 
-    # This checks if the user hits any escape button to close the program
     def check_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                pygame.quit()
+        self.global_trigger = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+            self.player.single_fire_event(event)
 
-    # This runs the main game loop
-    def main(self):
+    def run(self):
         while True:
             self.check_events()
             self.update()
             self.draw()
 
-# This sets the main to the game
+
 if __name__ == '__main__':
     game = Game()
-    game.main()
+    game.run()
