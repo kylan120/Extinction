72468192733294f8e9ec6509dcfc57d8f6c8be58
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2023-11-08 13:29:37 -0600

    even shorter enemy sounds

--- sound.py
+++ sound.py
@@ -8,7 +8,7 @@ class Sound:
         self.path = 'Resources/sounds/'
         self.gun = pygame.mixer.Sound(self.path + 'pew.wav')
         self.theme = pygame.mixer.Sound(self.path + 'extinction_theme.wav')
-        self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain_shorter.wav')
-        self.npc_attack = pygame.mixer.Sound(self.path + 'npc_attack_shorter.wav')
-        self.npc_death = pygame.mixer.Sound(self.path + 'npc_death_shorter.wav')
+        self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_attack = pygame.mixer.Sound(self.path + 'npc_attack.wav')
+        self.npc_death = pygame.mixer.Sound(self.path + 'npc_death.wav')
         self.player_pain = pygame.mixer.Sound(self.path + 'oof.wav')
