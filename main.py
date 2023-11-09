931203e47fea65d9efd756728805c62ae389f8ef
Nicholas Woerner <nicholaswoerner@gmail.com>
2023-11-08 19:26:06 -0600

    spacing and tabbing

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Game:
         pygame.display.set_caption("Extinction")
         pygame.display.flip()
         self.delta_time = self.clock.tick(FPS)
-       # self.sound.theme.play()
+        # self.sound.theme.play()
 
     def draw(self):
         self.object_renderer.draw()
